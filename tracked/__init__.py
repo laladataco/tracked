500d1c6794b020e542fcf291c4ab8e5ae28dd4c5
Jason Kriss <jasonkriss@gmail.com>
2021-03-06 21:59:54 -0800

    add ability to pass predefined name to run

--- tracked/__init__.py
+++ tracked/__init__.py
@@ -3,16 +3,16 @@ from typing import Callable, Tuple
 from tracked.utils import generate_name, current_timestamp
 
 
-def run(store, family, callable: Callable[[dict], Tuple[dict, dict]], *args, **kwargs):
-    name = generate_name(family)
+def run(store, family, callable: Callable[..., Tuple[dict, dict]], name=None, **kwargs):
+    if name is None:
+        name = generate_name(family)
     started = current_timestamp()
-    assets, evaluation = callable(*args, **kwargs)
+    assets, evaluation = callable(**kwargs)
     ended = current_timestamp()
     return store.push({
         'name': name,
         'started': started,
         'ended': ended,
-        'args': args,
         'kwargs': kwargs,
         'assets': assets,
         'evaluation': evaluation
